227f0277e6c28208c0caaa35f3f2c314410fc25d
Caio Lucas <lienscarlet@gmail.com>
2022-07-23 09:37:55 -0300

    feat: add DefaultLayout and router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Header } from './components/Header'
+import { Router } from './Router'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
@@ -7,7 +8,10 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <Header />
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
