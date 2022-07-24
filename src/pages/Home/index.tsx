caa1e5bd5112847ca112a5ca0be99c92fa2f0b72
Caio Lucas <lienscarlet@gmail.com>
2022-07-24 11:59:55 -0300

    feat: add Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,85 @@
+import { Coffee, IconProps, Package, ShoppingCart, Timer } from 'phosphor-react'
+import {
+  Benefit,
+  BenefitsContainer,
+  CoffeesContainer,
+  Greetings,
+  Headline,
+  HomeContainer,
+  Variant,
+} from './styles'
+
+import { FunctionComponent } from 'react'
+import CoffeeHeroPng from '../../assets/coffee-hero.png'
+import { CoffeeCard } from './components/CoffeeCard'
+
+interface BenefitProps {
+  text: string
+  icon: FunctionComponent<IconProps>
+  variant: Variant
+}
+
+const benefits: BenefitProps[] = [
+  {
+    text: 'Compra simples e segura',
+    icon: ShoppingCart,
+    variant: 'yellow-dark',
+  },
+  {
+    text: 'Embalagem mantém o café intacto',
+    icon: Package,
+    variant: 'base-text',
+  },
+  {
+    text: 'Entrega rápida e rastreada',
+    icon: Timer,
+    variant: 'yellow',
+  },
+  {
+    text: 'O café chega fresquinho até você',
+    icon: Coffee,
+    variant: 'purple',
+  },
+]
+
 export function Home() {
-  return <div></div>
+  return (
+    <HomeContainer>
+      <Greetings>
+        <div>
+          <Headline>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </p>
+          </Headline>
+
+          <BenefitsContainer>
+            {benefits.map(({ text, variant, icon: Icon }) => (
+              <Benefit key={text} variant={variant}>
+                <span>
+                  <Icon weight="fill" />
+                </span>
+                {text}
+              </Benefit>
+            ))}
+          </BenefitsContainer>
+        </div>
+        <img
+          src={CoffeeHeroPng}
+          alt="Coffee Delivery cup of coffee with beans around"
+        />
+      </Greetings>
+
+      <h2>Nossos cafés</h2>
+      <CoffeesContainer>
+        <CoffeeCard />
+        <CoffeeCard />
+        <CoffeeCard />
+        <CoffeeCard />
+        <CoffeeCard />
+      </CoffeesContainer>
+    </HomeContainer>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,68 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.div``
+export type Variant = 'yellow-dark' | 'yellow' | 'base-text' | 'purple'
+
+export interface BenefitProps {
+  variant: Variant
+}
+
+export const HomeContainer = styled.main``
+
+export const Greetings = styled.div`
+  display: flex;
+  gap: 3.5rem;
+  margin: 5.75rem 0;
+
+  img {
+    width: 29.75rem;
+    height: 22.5rem;
+  }
+`
+
+export const Headline = styled.div`
+  h1 {
+    font-size: 3rem;
+    color: ${({ theme }) => theme['base-title']};
+  }
+
+  p {
+    color: ${({ theme }) => theme['base-subtitle']};
+    font-size: 1.25rem;
+  }
+`
+
+export const BenefitsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  row-gap: 1.25rem;
+
+  margin-top: 4.125rem;
+`
+
+export const Benefit = styled.p<BenefitProps>`
+  display: flex;
+  align-items: center;
+
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: ${({ theme }) => theme.white};
+
+    height: 2rem;
+    width: 2rem;
+    border-radius: 1rem;
+
+    background-color: ${({ theme, variant }) => theme[variant]};
+
+    margin-right: 0.75rem;
+  }
+`
+
+export const CoffeesContainer = styled.div`
+  margin-top: 4.625rem;
+
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 2.5rem 2rem;
+`
