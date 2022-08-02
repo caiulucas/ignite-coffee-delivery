74f3e44cd7fa4cbb0a775fd11ebc4dcf4c09ee27
Caio Lucas <lienscarlet@gmail.com>
2022-08-02 17:04:03 -0300

    feat: add review order route

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { ConfirmOrder } from './pages/ConfirmOrder'
 import { Home } from './pages/Home'
+import { ReviewOrder } from './pages/ReviewOrder'
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/confirm-order" element={<ConfirmOrder />} />
+        <Route path="/review-order" element={<ReviewOrder />} />
       </Route>
     </Routes>
   )
