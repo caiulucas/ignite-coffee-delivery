d87ebd73f0e16d1720e8b15affe3eaa7c256ef1a
Caio Lucas <lienscarlet@gmail.com>
2022-08-09 11:02:28 -0300

    style: remove unused styles

--- src/pages/ConfirmOrder/styles.ts
+++ src/pages/ConfirmOrder/styles.ts
@@ -117,65 +117,6 @@ export const SelectedCoffees = styled.div`
 
 export const CoffeeList = styled.ul``
 
-export const Coffee = styled.div`
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid ${({ theme }) => theme['base-button']};
-  padding-bottom: 1.5rem;
-
-  img {
-    width: 4rem;
-    height: 4rem;
-  }
-
-  span {
-    font-size: 1rem;
-    font-weight: 700;
-    color: ${({ theme }) => theme['base-text']};
-  }
-
-  & + & {
-    padding-top: 1.5rem;
-  }
-`
-
-export const CoffeeInfo = styled.div`
-  display: flex;
-  gap: 1.25rem;
-
-  > div {
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-  }
-`
-export const CoffeeController = styled.div`
-  display: flex;
-  gap: 0.5rem;
-
-  > button {
-    background-color: ${({ theme }) => theme['base-button']};
-    border: none;
-    border-radius: 6px;
-    height: 2rem;
-    padding: 0 0.5rem;
-
-    display: flex;
-    align-items: center;
-    gap: 0.25rem;
-
-    font-size: 0.75rem;
-    text-transform: uppercase;
-    color: ${({ theme }) => theme['base-text']};
-
-    svg {
-      width: 1rem;
-      height: 1rem;
-      color: ${({ theme }) => theme.purple};
-    }
-  }
-`
-
 export const Pricing = styled.div`
   display: flex;
   flex-direction: column;
